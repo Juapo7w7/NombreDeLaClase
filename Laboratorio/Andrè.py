a43ecdeb0335f6aab13aa3578606206a4751ee06
André <Oivermolina@gmail.com>
2023-03-20 11:47:23 -0600

    Mensaje aleatoreo

---
+++ Laboratorio/Andrè.py
@@ -0,0 +1,20 @@
+edad=16
+print (edad)
+
+print (type(edad))
+
+nombre="Oliver"
+print (nombre)
+
+print (type(nombre))
+
+pi=3.1416
+print (pi)
+
+print (type(pi))
+van_a_ganar_el_ano=False
+
+print (van_a_ganar_el_ano)
+
+print (type(van_a_ganar_el_ano))
+ 
