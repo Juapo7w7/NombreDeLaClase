621c4a98d75e59ce7538d0d1c681171eaf05025b
André <Oivermolina@gmail.com>
2023-03-22 08:38:01 -0600

    se agrego un texto random

--- Laboratorio/Andrè.py
+++ Laboratorio/Andrè.py
@@ -12,9 +12,30 @@ pi=3.1416
 print (pi)
 
 print (type(pi))
+
 van_a_ganar_el_ano=False
 
 print (van_a_ganar_el_ano)
 
 print (type(van_a_ganar_el_ano))
- 
+
+DIASDLS=7
+
+print (DIASDLS)
+
+print (f"Mi nombre es {nombre}")
+
+#Esta variable me enseña a unir dos variables
+'''Hooooy te mando estas 25 Flores
+Recivelas mujer no digas no'''
+
+no1=1
+no2=2
+resultado= no1 + no2
+print (resultado)
+resultado2= resultado - no1
+print (resultado2)
+resultado3= resultado2 * 2
+print (resultado3)
+modulo= 125 %2
+print (modulo)
